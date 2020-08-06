726ab4212ca70658fea178b9a394bb93bd7e1b05
Augustin Bégué <augustin@begue.cc>
2020-08-06 23:31:02 +0200

    build

--- src/ts/control/data/parseXml.ts
+++
@@ -1,6 +0,0 @@
-import { parseGpxData } from "./parseGpxData";
-
-export function parseXml(xmlstr: string) {
-  const doc = new DOMParser().parseFromString(xmlstr, "text/xml");
-  return parseGpxData(doc.documentElement);
-}
--- src/ts/leaflet/animation/pauseVisualisation.ts
+++
@@ -1,9 +0,0 @@
-export function pauseVisualisation() {
-  const timeoutId = <string>localStorage.getItem("timeout");
-  clearTimeout(parseInt(timeoutId));
-
-  const centerCursor = document.getElementById("centerCursor");
-  if (!centerCursor)
-    throw new Error("centerCursor missing");
-  centerCursor.style.display = "none";
-}
