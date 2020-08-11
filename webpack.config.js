3e6abe17b493e5d15e4fe0d82ee85337f2f68a6c
Augustin Bégué <augustin@begue.cc>
2020-08-11 19:10:13 +0200

    added webpack js compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
-var path = require('path');
+var path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    // Change to your "entry-point".
-    entry: './src/',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [
-            { test: /\.(ts|js)x?$/, exclude: /node_modules/, loader: 'babel-loader', },
-            { test: /\.css$/, use: 'css-loader' },
-            { test: /\.svg$/, use: 'url-loader' },
-        ],
-    }
+  // Change to your "entry-point".
+  entry: {
+    index: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    chunkFilename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.(ts|js)x?$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, use: "css-loader" },
+      { test: /\.svg$/, use: "url-loader" },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg)$/,
+    }),
+  ],
 };
