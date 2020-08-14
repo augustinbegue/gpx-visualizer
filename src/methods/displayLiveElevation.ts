8bd24c52ef2d644c0e731c44bcfc69f015cfe377
Augustin Bégué <augustin@begue.cc>
2020-08-14 09:57:10 +0200

    deletions

--- src/methods/displayLiveElevation.ts
+++
@@ -1,8 +0,0 @@
-
-export function displayLiveElevation(computedElevation: number, duration: number, animationSpeed: number) {
-  const elevationSpan = document.getElementById("elevation");
-
-  if (duration > 0.2 / animationSpeed && elevationSpan) {
-    elevationSpan.innerHTML = computedElevation.toString();
-  }
-}
